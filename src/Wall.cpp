dd92fede90e0c870f791ea9f07679968cf28d350
diegomacario <diego.macario5@gmail.com>
2022-07-30 09:47:52 -0400

    Fix bug in the scale of things.

--- src/Wall.cpp
+++ src/Wall.cpp
@@ -17,13 +17,13 @@ Wall::Wall(const glm::vec3& position,
 
 glm::mat4 Wall::getModelMatrix() const
 {
-   // 3) Translate the cube
+   // 3) Translate the plane
    glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), mPosition);
 
-   // 2) Rotate the cube
+   // 2) Rotate the plane
    modelMatrix = modelMatrix * glm::mat4(mOrientation);
 
-   // 1) Scale the cube
+   // 1) Scale the plane
    modelMatrix = glm::scale(modelMatrix, glm::vec3(mWidth, mHeight, 1.0f));
 
    return modelMatrix;
--- src/World.cpp
+++ src/World.cpp
@@ -20,14 +20,14 @@ void World::initializeRigidBodies()
    orientation[1][1] =  glm::cos(theta);
    orientation[2][2] = 1;
 
-   mRigidBodies.emplace_back(1.0f,             // Mass
-                             0.25f,            // Width
-                             0.25f,            // Height
-                             0.25f,            // Depth
-                             0.5f,             // Coefficient of restitution
+   mRigidBodies.emplace_back(1.0f, // Mass
+                             0.25f, // Width
+                             0.25f, // Height
+                             0.25f, // Depth
+                             0.75f, // Coefficient of restitution
                              glm::vec3(0.0f, 0.0f, 0.0f), // Position of CM
-                             orientation,  // Orientation
-                             glm::vec3(-1.0f, 0.0f, -1.0f),  // Velocity of CM,
+                             orientation, // Orientation
+                             glm::vec3(-1.0f, 0.0f, -1.0f), // Velocity of CM,
                              glm::vec3(0.0f)); // Angular momentum
 }
 
@@ -35,27 +35,27 @@ void World::initializeWalls()
 {
    // Right
    glm::mat3 orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(2.5f, 0.0f, 0.0f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(2.5f * 0.5f, 0.0f, 0.0f), orientation, 2.5f, 2.5f);
 
    // Left
    orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(-2.5f, 0.0f, 0.0f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(-2.5f * 0.5f, 0.0f, 0.0f), orientation, 2.5f, 2.5f);
 
    // Front
    orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(0.0f, 0.0f, 2.5f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(0.0f, 0.0f, 2.5f * 0.5f), orientation, 2.5f, 2.5f);
 
    // Back
    orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(0.0f, 0.0f, -2.5f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(0.0f, 0.0f, -2.5f * 0.5f), orientation, 2.5f, 2.5f);
 
    // Top
    orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(0.0f, 2.5f, 0.0f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(0.0f, 2.5f * 0.5f, 0.0f), orientation, 2.5f, 2.5f);
 
    // Bottom
    orientation = glm::mat3(Q::quatToMat4(Q::lookRotation(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
-   mWalls.emplace_back(glm::vec3(0.0f, -2.5f, 0.0f), orientation, 2.5f, 2.5f);
+   mWalls.emplace_back(glm::vec3(0.0f, -2.5f * 0.5f, 0.0f), orientation, 2.5f, 2.5f);
 }
 
 bool World::simulate(float deltaTime)
